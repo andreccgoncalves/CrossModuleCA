6f7075350c53f4057e5bc23f4e4fb4d9960440b5
andreccgoncalves <72031101+andreccgoncalves@users.noreply.github.com>
2021-12-18 19:31:28 +0000

    2x2
    2x2 working

---
+++ src/cajavamatrices/start/CaJavaMatrices.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cajavamatrices.start;
+
+import database.DbInteraction;
+
+
+/**
+ *
+ * @author Jefferson Pereira de Matos 2019385
+ * @author Andre Goncalves 2020195
+ */
+public class CaJavaMatrices {
+
+
+    public static void main(String[] args) throws Exception  {
+        
+        /*
+        My main class creates an object of the type DbInteraction
+        which will access its constructor and run its first method that will be calling other methods according to the necessity. 
+        */
+        DbInteraction db = new DbInteraction();
+
+
+           
+
+                
+        
+
+        
+        
+    }
+    
+}
